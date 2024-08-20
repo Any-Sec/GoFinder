1ed58386a9d3f63955b9fed21c154e5c8e7b9b05
penguinuxx <ahmetyavuzkanat@outlook.com>
2024-08-20 10:11:28 +0300

    added percent

--- pkgs/result.go
+++ pkgs/result.go
@@ -9,25 +9,25 @@ import (
 
 
 func InfosMenu(url_addres string , file_path string){
-	color.White("===============GoFinder======================")
-	color.White("[#] Url      :%s \n", url_addres)
-	color.White("[#] Wordlist :%s \n", file_path)
-	color.White("=============================================")
+	color.White("===============GoFinder======================\n\n")
+	color.White("[#] Url      :   %s \n", url_addres)
+	color.White("[#] Wordlist :   %s \n\n", file_path)
+	color.White("=============================================\n\n")
 	lines := FileReader(file_path)
 	for i, value := range lines {
 		NewUrl := url_addres+value
 		data , _ := GetRequestToWebsite(NewUrl)
 		if data.StatusCode != 404 {
-			color.Green("[FOUND] url: %s [%s] [%s] \n",NewUrl,data.Proto,data.Status)
+			color.Green("[*][FOUND]  %s [%s] [%s] %.2f%% \n",NewUrl,data.Proto,data.Status,CounterLines(float32(i),float32(len(lines))))
 		}
-		if i%100 == 0 {
-			CounterLines(i,len(lines))	
-		}
+
 		
 	}
 }
 
-func CounterLines(index int, lines int)  {
-	color.Green("Lines : %d / %d ", index,lines)
+func CounterLines(index float32, lines float32) float32 {
+
+	percent := (index/lines) * 100
+	return percent
 
 }
